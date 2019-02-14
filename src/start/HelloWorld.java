c5f3d2eb5f654f4c6063c2d2e0c2366a183096a6
dariasmetek <daria.smetek@gmail.com>
2019-02-14 10:07:12 +0100

    START

---
+++ src/start/HelloWorld.java
@@ -0,0 +1,11 @@
+package start;
+//Komentarz
+//psvm - public static void main(String[] args)
+//sout - System.out.println();
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
+
+
